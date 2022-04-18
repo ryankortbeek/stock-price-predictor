bb0c83a612eeaf20149342f0fd37ee317bbe14c9
Ryan Kortbeek <rakortbe@ualberta.ca>
2022-04-18 10:54:52 -0600

    Second attempt at implementing mlp from scratch

--- multi_layer_perceptron.py
+++ multi_layer_perceptron.py
@@ -1,6 +1,4 @@
-import sys
 import numpy as np
-# https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
 
 
 def init_neural_network(dims):
@@ -10,17 +8,13 @@ def init_neural_network(dims):
     w_l1: l1_dim x l2_dim
     w_l2: l2_dim x 1
     '''
-    if len(dims) < 3:
-        print('For the sake of this application, neural network must have at least three layers (one input, one hidden, one output).', file=sys.stderr)
-        sys.exit(1)
-
     w = [np.random.rand(dims[i], dims[i + 1]) for i in range(len(dims) - 1)]
     return w
 
 
 def do_forward_propagation(X, w):
     '''
-    Performs forward propagation. Iterates over the layers in the neural network 
+    Performs forward propagation. Iterates over the layers in the neural network
     and computes the prediction.
     '''
     def sigmoid(z):
@@ -40,9 +34,10 @@ def calc_loss_and_risk(Ys, t, loss_as_vec=False):
     t = np.reshape(t, [t.shape[0], 1])
 
     # Mean square error
-    loss = (1 / len(Ys[-1])) * (Ys[-1] - t) ** 2 if loss_as_vec else (1 / len(Ys[-1])) * np.linalg.norm(Ys[-1] - t, 2) ** 2
+    loss = (1 / Ys[-1].shape[0]) * (Ys[-1] - t) ** 2 if loss_as_vec else (1 / \
+            Ys[-1].shape[0]) * np.linalg.norm(Ys[-1] - t, 2) ** 2
     # Mean absolute difference
-    risk = (1 / len(Ys[-1])) * np.linalg.norm(np.absolute(Ys[-1] - t), 1)
+    risk = (1 / Ys[-1].shape[0]) * np.linalg.norm(np.absolute(Ys[-1] - t), 1)
     return loss, risk
 
 
@@ -58,7 +53,8 @@ def do_backward_propagation(w, Ys, t):
 
     dws = [loss * sigmoid_derivative_v(Ys[-1])]
     for i in range(len(w) - 1, 0, -1):
-        dws.append(np.dot(dws[-1], np.transpose(w[i])) * sigmoid_derivative_v(Ys[i]))
+        dws.append(np.dot(dws[-1], np.transpose(w[i]))
+                   * sigmoid_derivative_v(Ys[i]))
     return dws, loss
 
 
@@ -80,19 +76,24 @@ def mlp_train(X_train, t_train, X_val, t_val, hyperparams):
         loss_this_epoch = 0
         for b in range(num_batches):
             # X_batch: batch_size x (d + 1)
-            X_batch = X_train[b * hyperparams.batch_size:(b + 1) * hyperparams.batch_size]
+            X_batch = X_train[b *
+                              hyperparams.batch_size:(b +
+                                                      1) *
+                              hyperparams.batch_size]
             # t_batch: batch_size x 1
-            t_batch = t_train[b * hyperparams.batch_size:(b + 1) * hyperparams.batch_size]
+            t_batch = t_train[b *
+                              hyperparams.batch_size:(b +
+                                                      1) *
+                              hyperparams.batch_size]
 
             Ys = do_forward_propagation(X_batch, w)
-
             dws, loss = do_backward_propagation(w, Ys, t_batch)
             loss_this_epoch += np.linalg.norm(loss, 1)
             # Mini-batch gradient descent (dws is in reverse order)
             for i in range(len(w)):
-                w[i] -= hyperparams.alpha * np.dot(np.transpose(Ys[i]), dws[-(i + 1)])
-            # Use l2 regularization
-            # w = w - hyperparams.alpha * (gradient + hyperparams.decay * w)
+                # Use l2 regularization
+                w[i] -= hyperparams.alpha * \
+                    (np.dot(np.transpose(Ys[i]), dws[-(i + 1)]) + hyperparams.decay * w[i])
 
         # Compute the training loss by averaging loss_this_epoch
         training_loss = loss_this_epoch / num_batches
@@ -107,4 +108,3 @@ def mlp_train(X_train, t_train, X_val, t_val, hyperparams):
             risk_best, epoch_best = risk, epoch
 
     return w_best, risk_best, epoch_best, losses_train, risks_val
-
